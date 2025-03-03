b15f197d29b2d0787f5f0bfba2a2dc6175636524
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2025-03-03 17:43:52 -0500

    Initial Commit

---
+++ QuoteFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuoteFinder
+//
+//  Created by Skye Willow Harris-Stoertz on 2025-03-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuoteFinder/QuoteFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteFinderApp.swift
+//  QuoteFinder
+//
+//  Created by Skye Willow Harris-Stoertz on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct QuoteFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
