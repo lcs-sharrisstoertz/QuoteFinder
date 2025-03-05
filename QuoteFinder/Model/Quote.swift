1b3b8eba88a722d1f51487bc11ac575dc7922eec
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2025-03-05 08:38:11 -0500

    Created code in Model, View Model, and View (mostly copy and paste).

--- QuoteFinder/Model/Quote.swift
+++ QuoteFinder/Model/Quote.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct Quote: Codable {
+    
+    // MARK: Stored properties
+    let quoteText: String
+    let quoteAuthor: String?
+}
+ 
+// Create an example quote for testing purposes
+let exampleQuote = Quote(
+    quoteText: "The beginning of knowledge is the discovery of something we do not understand.",
+    quoteAuthor: "Frank Herbert"
+
+)
--- QuoteFinder/View/QuoteView.swift
+++ QuoteFinder/View/QuoteView.swift
@@ -8,17 +8,93 @@
 import SwiftUI
 
 struct QuoteView: View {
+    
+    // MARK: Stored properties
+    
+    // Create the view model (temporarily show the default quote)
+    @State var viewModel = QuoteViewModel()
+    
+    // Controls author visibility
+    @State var quoteAuthorOpacity = 0.0
+    
+    // Controls button visibility
+    @State var buttonOpacity = 0.0
+    
+    // Starts a timer to wait on revealing author
+    @State var quoteAuthorTimer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+    
+    // Starts a timer to wait on revealing button to get new joke
+    @State var buttonTimer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+
+    
+    // MARK: Computed properties
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            // Show a quote if one exists
+            if let currentQuote = viewModel.currentQuote {
+                
+                Group {
+                    Text(currentQuote.setup ?? "")
+                        .padding(.bottom, 100)
+                    
+                    Text(currentQuote.quoteAuthor ?? "")
+                        .opacity(quoteAuthorOpacity)
+                        .onReceive(quoteAuthorTimer) { _ in
+                            
+                            withAnimation {
+                                quoteAuthorOpacity = 1.0
+                            }
+                            
+                            // Stop the timer
+                            quoteAuthorTimer.upstream.connect().cancel()
+                        }
+ 
+                }
+                .font(.title)
+                .multilineTextAlignment(.center)
+                
+                Button {
+                 
+                    // Hide author and button
+                    withAnimation {
+                        viewModel.currentJoke = nil
+                        quoteAuthorOpacity = 0.0
+                        buttonOpacity = 0.0
+                    }
+                                        
+                    // Get a new quote
+                    Task {
+                        await viewModel.fetchQuote()
+                    }
+                    
+                    // Restart timers
+                    quoteAuthorTimer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+                    buttonTimer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+                    
+                } label: {
+                 
+                    Text("New Quote")
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .opacity(buttonOpacity)
+                .onReceive(buttonTimer) { _ in
+                    
+                    withAnimation {
+                        buttonOpacity = 1.0
+                    }
+                    
+                    // Stop the timer
+                    buttonTimer.upstream.connect().cancel()
+                }
+                
+            }
+            
         }
-        .padding()
     }
 }
-
+ 
 #Preview {
     QuoteView()
 }
